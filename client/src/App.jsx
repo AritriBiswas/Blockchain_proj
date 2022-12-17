df3f33fefbb878aeb265f9efce55443f05fdfda3
AritriBiswas <aritribiswas07@gmail.com>
2022-12-17 14:27:55 +0530

    landing and funds page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import { EthProvider } from "./contexts/EthContext";
 import Navbar from "./components/Navbar/Navbar"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage"
 import "./App.css";
-import LandingPage from "./pages/LandingPage/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateFundPage from "./pages/CreateFundPage/CreateFundPage";
+
 
 function App() {
   return (
     <EthProvider>
-    
+      <BrowserRouter>
         <Navbar/>
-    
+
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/createfund" element={<CreateFundPage/>}/>
+        </Routes>
+      </BrowserRouter>
       
      
   </EthProvider>
