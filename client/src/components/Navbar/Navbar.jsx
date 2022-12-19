bde5b654a848e99f0d28c658878f6d4db9d4b0ec
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2022-12-19 13:00:32 +0530

    LANDING PAGE AND ONGOING PAGE ADDED

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import "./Navbar.css"
+import useEth from '../../contexts/EthContext/useEth';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+const Navbar = ({path}) => {
+  const [address,setAddress] = useState("none");
+  const {state} = useEth();
+  useEffect(() => {
+    let curraddress = "none";
+    if(state.accounts) curraddress =  state.accounts[0];
+    setAddress(curraddress);
+    // console.log(state.accounts);
+  }, [state.accounts])
+  
   return (
-    <nav className="navbar">
-          
+      <nav className="navbar">
+          <img src={path} alt="eth-raiser" className="logo" />
           <ul className='navList'>
-          <li>
+              <li>
                   <Link to="/">Home</Link>
               </li>
               <li>
@@ -16,8 +28,9 @@ export default function Navbar() {
               <li>
                   <Link to="/createfund">Create Charity</Link>
               </li>
-             
           </ul>
       </nav>
-  )
+  );
 }
+
+export default Navbar
